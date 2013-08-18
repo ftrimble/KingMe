feb2587ea8f907126c54ff9913df34c933f38f18
ftrimble <trimble.forest@gmail.com>
2013-08-17 22:21:39 -0400

    Initialized the Android project

---
+++ src/ftrimble/kingme/device/KingMe.java
@@ -0,0 +1,15 @@
+package ftrimble.kingme.device;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KingMe extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
