5779e6ee97d656d65f43d175553299597a2c6548
ftrimble <trimble.forest@gmail.com>
2014-04-17 21:58:04 -0400

    Set it up to show maps on the feed
    Still need to overlay gpx tracks on the maps so that the
    
    maps are actually meaningful.

--- web/django-root/rideupload/urls.py
+++ web/django-root/rideupload/urls.py
@@ -3,3 +3,5 @@ from django.conf.urls import patterns, url
 from rideupload import views
 
 urlpatterns = patterns('', url(r'^$', views.feed, name='feed'))
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
